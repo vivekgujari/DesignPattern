c30c158b9391dee78401f3e637ca6a5825a3c918
Vivek Gujari <vivekgujari@gmail.com>
2021-11-12 09:54:26 -0700

    Added factory design pattern and notes.

---
+++ DesignPattern/CalculatorFactoryPattern.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern
+{
+    public class CalculatorFactoryPattern
+    {
+        class Add : ICalculate
+        {
+            public int Calculate(int a, int b)
+            {
+                return a + b;
+            }
+        }
+
+        class Subtract : ICalculate
+        {
+            public int Calculate(int a, int b)
+            {
+                return a - b;
+            }
+        }
+
+        public ICalculate Calculate(string input)
+        {
+            ICalculate obj = null;
+            if (input.ToLower().Equals("add"))
+            {
+                obj = new Add();
+            }
+            else if (input.ToLower().Equals("subtract"))
+            {
+                obj = new Subtract();
+            }
+            return obj;
+        }
+    }
+}
--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -4,9 +4,18 @@ namespace DesignPattern
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter two numbers");
+            int a = Int32.Parse(Console.ReadLine());
+            int b = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Do you want to add or subtract");
+            string input = Console.ReadLine();
+            CalculatorFactoryPattern factory = new CalculatorFactoryPattern();
+            ICalculate obj = factory.Calculate(input);
+            Console.WriteLine("The {0} of {1} and {2} is {3}", input, a, b, obj.Calculate(a, b));
+            Console.WriteLine("End of main program");
         }
     }
 }
