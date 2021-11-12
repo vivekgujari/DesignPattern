71f7c34dffe697a5b36aeb46c6f1a36ecb1f3f83
Vivek Gujari <vivekgujari@gmail.com>
2021-11-12 11:16:08 -0700

    Added singleton desing pattern

---
+++ DesignPattern/LoggerSingleton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern
+{
+    public class LoggerSingleton
+    {
+        public static LoggerSingleton Logger;
+        private LoggerSingleton()
+        { 
+            
+        }
+
+        public static LoggerSingleton getInstance()
+        {
+            if (Logger == null)
+                Logger = new LoggerSingleton();
+            return Logger;
+        }
+    }
+}
